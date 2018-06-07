34eed4964c6449b6db6387c4dbc24f720452748e
Jelledegraaf <39087189+Jelledegraaf@users.noreply.github.com>
2018-06-07 12:39:06 +0200

    melding weer gefixt

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,15 +10,31 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
-     */
+   * @Route("/", name="homepage")
+   */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('SELECT a FROM AppBundle:Artikel a WHERE a.voorraad < a.minimumVoorraad');
+        $artikelen = $query->getResult();
+
+        // Als er artikelen zijn die een te lage voorraad hebben..
+        if(sizeof($artikelen) > 0) { // Meerdere artikelen gevonden
+          if(sizeof($artikelen) == 1) { // Een artikel gevonden
+            $melding = "Er is een artikel onder de minimum voorraad!";
+          } else {
+            $melding = "Er zijn artikelen die onder de minimum voorraad zijn!";
+          }
+        } else { // Geen artikelen gevonden
+          $melding = "";
+        }
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'melding' => $melding,
         ]);
-    }
+      }
 
     /**
      * @Route("/admin")
